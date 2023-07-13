2b6c8f7c7587227c80927637396719da97a4b121
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-07-12 23:13:24 -0400

    fix: Update scrape API route and visitor page
    - Updated the import statement for `headers` in the `api/scrape/route.js` file.
    - Removed the unnecessary code related to `ip` in the `api/scrape/route.js` file.
    - Updated the console log statement in the `api/scrape/route.js` file.
    - Updated the title and description metadata in the `layout.js` file.
    - Removed the unnecessary code related to `ip` in the `visitor/page.js` file.
    - Added a new client-side fetch request to the `visitor/page.js` file to get the visitor's IP address from the `/api/scrape/` route and display it on the page.

--- app/api/scrape/route.js
+++ app/api/scrape/route.js
@@ -1,55 +1,28 @@
 import { NextResponse } from "next/server";
-import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { FaissStore } from "langchain/vectorstores/faiss";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-// import {} from 'hnswlib-node'
+import { headers } from 'next/headers'
 
+export async function GET(req) {
+  // const { searchParams } = new URL(request.url);
 
-export async function GET(request) {
-  const { searchParams } = new URL(request.url);
+  let ip;
 
-  const url = searchParams.get("url");
+  // const { req } = request;
+  const headersList = headers()
+  const referer = headersList.get('x-forwarded-for')
 
-  /**
-   * Loader uses `page.evaluate(() => document.body.innerHTML)`
-   * as default evaluate function
-   **/
-  const loader = new PuppeteerWebBaseLoader("https://www.tabnews.com.br/");
+  // if (req.headers['x-forwarded-for']) {
+  //   ip = req.headers['x-forwarded-for'].split(',')[0];
+  // } else if (req.headers['x-real-ip']) {
+  //   ip = req;
+  // } else {
+  //   ip = req;
+  // }
 
-  const docs = await loader.load();
-
-  //   console.log(docs[0].pageContent);
-
-  const splitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
-    chunkSize: 175,
-    chunkOverlap: 20,
-  });
-
-  const documents = await splitter.createDocuments([docs[0].pageContent]);
-
-  const vectorStore = await HNSWLib.fromDocuments(
-    documents,
-    new OpenAIEmbeddings(),
-    // {
-    //   index: "Flat",
-    //   indexParams: {
-    //     dimension: 768,
-    //     metricType: "L2",
-    //   },
-    // }
-  );
-
-  const results = await vectorStore.similaritySearch("hello world", {
-    limit: 10,
-  });
-
-  console.log(results);
+  console.log(referer);
 
   return NextResponse.json({
     body: {
-      message: "Hello from the server",
+      message: referer,
     },
   });
 }
--- app/visitor/page.js
+++ app/visitor/page.js
@@ -1,31 +1,37 @@
-export default function page(props) {
-    const ip = props.ip;
+'use client'
+
+import { useEffect, useState } from "react";
+
+
+export default function page() {
+
+    const [data, setdata] = useState(null)
+
+    useEffect(() => {
+
+        
+        async function getData() {
+            // The return value is *not* serialized
+            const res = await fetch('api/scrape/');
+            // You can return Date, Map, Set, etc.
+            return res.json();
+        }
+
+        getData().then((res) => {  
+            setdata(res)
+        }
+        );
+
+    },[])
+
+
+
     return (
       <div style={{ width: '100vw', height: '100vh', padding: 30 }}>
         <h1>
-          IP Address <span style={{ color: 'blue' }}>{ip}</span>
+          IP Address <span style={{ }}>{data?.body.message}</span>
         </h1>
       </div>
     );
   }
   
-  export async function getServerSideProps(context) {
-    let ip;
-  
-    const { req } = context;
-  
-    if (req.headers['x-forwarded-for']) {
-      ip = req.headers['x-forwarded-for'].split(',')[0];
-    } else if (req.headers['x-real-ip']) {
-      ip = req.connection.remoteAddress;
-    } else {
-      ip = req.connection.remoteAddress;
-    }
-  
-    console.log(ip);
-    return {
-      props: {
-        ip,
-      },
-    };
-  }
