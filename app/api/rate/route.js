c8271e0d98085de73643f91b49944359f48693e7
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-07-16 23:52:27 -0400

    Add email sending functionality and schedule it to run every 5 minutes
    - Add nodemailer library for sending emails.
    - Configure nodemailer transporter with necessary settings.
    - Write a function to send an email using the defined transporter.
    - Call the email sending function in the GET endpoint.
    - Add node-cron library for scheduling the email sending.
    - Add a vercel.json file to configure the cron job to run the GET endpoint every 5 minutes.
    - Update package.json to include nodemailer and node-cron dependencies.

---
+++ app/api/rate/route.js
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.zoho.com",
+//   secure: false,
+  port: 465,
+  auth: {
+    user: "mdhabiborrahman@a2zzz.com",
+    pass: "82fQHM6iQzg0",
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
+  },
+});
+
+async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"A2ZZZ 🙋🏻‍♂️" <mdhabiborrahman@a2zzz.com>', // sender address
+      to: "mdhera211@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    //
+    // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+    //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+    //       <https://github.com/forwardemail/preview-email>
+    //
+  }
+
+export async function GET() {
+
+    main().catch(console.error);
+
+
+  return NextResponse.json({
+    body: {
+      message: "hello",
+    },
+  });
+}
