38bc3d925f6ff9f3ddc9419177cac16852e0ea72
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-07-17 01:39:06 -0400

    feat: Add web scraping functionality with puppeteer
    This commit adds the ability to scrape data from a specific website using the puppeteer library. The code launches a headless browser, navigates to a URL, waits for specific elements to load, and then retrieves the desired data. The scraped data is then used to send an email with nodemailer. The email includes the scraped data in both plain text and HTML formats. The commit also updates the email subject and body to reflect the specific currency conversion rate being scraped.

--- app/api/rate/route.js
+++ app/api/rate/route.js
@@ -1,35 +1,87 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import puppeteer from "puppeteer";
 
 
-export async function POST() {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.zoho.com",
-      secure: true,
-      port: 465,
-      auth: {
-        user: "mdhabiborrahman@a2zzz.com",
-        pass: "82fQHM6iQzg0",
-      },
-      tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false,
-      },
-    });
+async function scrape() {
+    const browser = await puppeteer.launch(
+        {
+            headless: true,
+            slowMo:100
+        }
+    );
+    const page = await browser.newPage();
+  
+    await page.goto("https://www.taptapsend.com/country-landing-pages/bangladesh");
+  
+    // Wait for the country selector and select Canada
+    await page.waitForSelector('#origin-currency');
+    await page.select('#origin-currency', 'CA');
+  
+    // Wait for the FX rate text to load
+    await page.waitForSelector('#fxRateText');
+
+
+  
+    // Get the price text
+    const priceElement = await page.$('#fxRateText');
+
+
+    
+    const price = await page.evaluate(element => element.textContent, priceElement);
+  
+    // Print the price
+    console.log('The price is:', price);
+  
+    await browser.close();
+  
+    return price;
+  }
+
+
+async function email(data) {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    secure: true,
+    port: 465,
+    auth: {
+      user: "mdhabiborrahman@a2zzz.com",
+      pass: "82fQHM6iQzg0",
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  });
 
   var info = await transporter.sendMail({
-    from: '"A2ZZZ 🙋🏻‍♂️" <mdhabiborrahman@a2zzz.com>', // sender address
+    from: '"A2ZZZ - bot 🙋🏻‍♂️" <mdhabiborrahman@a2zzz.com>', // sender address
     to: "mdhera211@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    subject: "TapTap rate cad to bdt ✔", // Subject line
+    text: data, // plain text body
+    html: `<b>${data}</b>`, // html body
   });
 
   console.log("Message sent: %s", info.messageId);
+  return info.messageId;
+}
+
+
+
+
+export async function POST() {
+    
+    const success = await scrape()
+    
+    const resemail = await email(success)
+
+
+
 
   return NextResponse.json({
     body: {
-      message: info.messageId,
+      message: resemail,
+      data:'cad to bdt rate now' + success,
     },
   });
 }
